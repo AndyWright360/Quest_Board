e50aa59e46b272d79190e7b8f9e72b109dde9a8d
AndyWright360 <>
2024-01-13 17:47:49 +0000

    docs: add user table to models.py

--- quest_board/models.py
+++ quest_board/models.py
@@ -1,1 +1,13 @@
 from quest_board import db
+
+
+class User(db.Model):
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.username
