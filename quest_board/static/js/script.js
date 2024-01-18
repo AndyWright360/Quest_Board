13d80adfce144ee751d4c1288045970069729449
AndyWright360 <>
2024-01-18 21:28:54 +0000

    feat: add event listsners to footer icons

--- quest_board/static/js/script.js
+++ quest_board/static/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+    // Initialise side navbar
     $(".button-collapse").sideNav();
 });
+
+// Create variable of footer icons
+const icons = document.getElementsByClassName("footer-icon");
+
+// Add the grow style class to target
+const grow = function () {
+    this.classList.add("grow");
+};
+
+// Remove the grow style class from target
+const shrink = function () {
+    this.classList.remove("grow");
+};
+
+// Loop through footer icons to apply event listeners
+for (let i = 0; i < icons.length; i++) {
+    icons[i].addEventListener("mouseenter", grow);
+    icons[i].addEventListener("mouseleave", shrink);
+};
