52fa062f36175df32e1ed6bfde81403497e5ad79
AndyWright360 <>
2024-01-26 17:36:09 +0000

    docs: customise map location and pins

--- quest_board/static/js/map.js
+++ quest_board/static/js/map.js
@@ -1,29 +1,33 @@
+/*
+The following code was obtained from https://developers.google.com/maps/documentation/javascript/examples/advanced-markers-html and has been modified.
+*/
+
 async function initMap() {
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const center = { lat: 37.43238031167444, lng: -122.16795397128632 };
+    const center = { lat: 51.562179, lng: -3.47715 };
     const map = new Map(document.getElementById("map"), {
-        zoom: 11,
+        zoom: 9,
         center,
         mapId: "4504f8b37365c3d0",
     });
 
-    for (const property of properties) {
+    for (const location of locations) {
         const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
             map,
-            content: buildContent(property),
-            position: property.position,
-            title: property.description,
+            content: buildContent(location),
+            position: location.position,
+            title: location.name,
         });
 
         AdvancedMarkerElement.addListener("click", () => {
-            toggleHighlight(AdvancedMarkerElement, property);
+            toggleHighlight(AdvancedMarkerElement, location);
         });
     }
 }
 
-function toggleHighlight(markerView, property) {
+function toggleHighlight(markerView, location) {
     if (markerView.content.classList.contains("highlight")) {
         markerView.content.classList.remove("highlight");
         markerView.zIndex = null;
@@ -33,171 +37,70 @@ function toggleHighlight(markerView, property) {
     }
 }
 
-function buildContent(property) {
+function buildContent(location) {
     const content = document.createElement("div");
 
-    content.classList.add("property");
+    content.classList.add("location");
     content.innerHTML = `
     <div class="icon">
-        <i aria-hidden="true" class="fa fa-icon fa-${property.type}" title="${property.type}"></i>
-        <span class="fa-sr-only">${property.type}</span>
+        <i aria-hidden="true" class="${location.icon}" title="${location.name}"></i>
+        <span class="fa-sr-only">${location.type}</span>
     </div>
     <div class="details">
-        <div class="price">${property.price}</div>
-        <div class="address">${property.address}</div>
-        <div class="features">
-        <div>
-            <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
-            <span class="fa-sr-only">bedroom</span>
-            <span>${property.bed}</span>
-        </div>
-        <div>
-            <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
-            <span class="fa-sr-only">bathroom</span>
-            <span>${property.bath}</span>
-        </div>
-        <div>
-            <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
-            <span class="fa-sr-only">size</span>
-            <span>${property.size} ft<sup>2</sup></span>
-        </div>
-        </div>
+        <div class="name">${location.name}</div>
+        <div class="address">${location.address}</div>
+        <div class="phone"><i class="fa-solid fa-phone"></i> ${location.phone}</div>
+        <div class="hours"><i class="fa-solid fa-clock"></i> Mon - Sun: 10am - 6pm</div>
     </div>
     `;
     return content;
 }
 
-const properties = [
+const locations = [
     {
-        address: "215 Emily St, MountainView, CA",
-        description: "Single family house with modern design",
-        price: "$ 3,889,000",
-        type: "home",
-        bed: 5,
-        bath: 4.5,
-        size: 300,
+        address: "45, Castle Arcade, Cardiff CF10 1BW",
+        name: "Castle Games",
+        icon: "fa-brands fa-fort-awesome",
+        type: "castle",
+        phone: "02921 123 456",
         position: {
-            lat: 37.50024109655184,
-            lng: -122.28528451834352,
+            lat: 51.4809315,
+            lng: -3.1817093,
         },
     },
     {
-        address: "108 Squirrel Ln &#128063;, Menlo Park, CA",
-        description: "Townhouse with friendly neighbors",
-        price: "$ 3,050,000",
-        type: "building",
-        bed: 4,
-        bath: 3,
-        size: 200,
+        address: "1 Austin Friars, Newport NP20 1DQ",
+        name: "Boarderlands",
+        icon: "fa-solid fa-chess-board",
+        type: "chessboard",
+        phone: "01633 123 456",
         position: {
-            lat: 37.44440882321596,
-            lng: -122.2160620727,
+            lat: 51.5867805,
+            lng: -2.996156,
         },
     },
     {
-        address: "100 Chris St, Portola Valley, CA",
-        description: "Spacious warehouse great for small business",
-        price: "$ 3,125,000",
-        type: "warehouse",
-        bed: 4,
-        bath: 4,
-        size: 800,
+        address: "7 The Rhiw, Bridgend CF31 3BL",
+        name: "Dungeons & Dice",
+        icon: "fa-solid fa-dice",
+        type: "dice",
+        phone: "01656 123 456",
         position: {
-            lat: 37.39561833718522,
-            lng: -122.21855116258479,
+            lat: 51.5053977,
+            lng: -3.5864474,
         },
     },
     {
-        address: "98 Aleh Ave, Palo Alto, CA",
-        description: "A lovely store on busy road",
-        price: "$ 4,225,000",
-        type: "store-alt",
-        bed: 2,
-        bath: 1,
-        size: 210,
+        address: "8 High St, Swansea SA1 1LE",
+        name: "Checkmates",
+        icon: "fa-solid fa-chess-king",
+        type: "king",
+        phone: "01792 123 456",
         position: {
-            lat: 37.423928529779644,
-            lng: -122.1087629822001,
+            lat: 51.6212663,
+            lng: -3.9459948,
         },
-    },
-    {
-        address: "2117 Su St, MountainView, CA",
-        description: "Single family house near golf club",
-        price: "$ 1,700,000",
-        type: "home",
-        bed: 4,
-        bath: 3,
-        size: 200,
-        position: {
-            lat: 37.40578635332598,
-            lng: -122.15043378466069,
-        },
-    },
-    {
-        address: "197 Alicia Dr, Santa Clara, CA",
-        description: "Multifloor large warehouse",
-        price: "$ 5,000,000",
-        type: "warehouse",
-        bed: 5,
-        bath: 4,
-        size: 700,
-        position: {
-            lat: 37.36399747905774,
-            lng: -122.10465384268522,
-        },
-    },
-    {
-        address: "700 Jose Ave, Sunnyvale, CA",
-        description: "3 storey townhouse with 2 car garage",
-        price: "$ 3,850,000",
-        type: "building",
-        bed: 4,
-        bath: 4,
-        size: 600,
-        position: {
-            lat: 37.38343706184458,
-            lng: -122.02340436985183,
-        },
-    },
-    {
-        address: "868 Will Ct, Cupertino, CA",
-        description: "Single family house in great school zone",
-        price: "$ 2,500,000",
-        type: "home",
-        bed: 3,
-        bath: 2,
-        size: 100,
-        position: {
-            lat: 37.34576403052,
-            lng: -122.04455090047453,
-        },
-    },
-    {
-        address: "655 Haylee St, Santa Clara, CA",
-        description: "2 storey store with large storage room",
-        price: "$ 2,500,000",
-        type: "store-alt",
-        bed: 3,
-        bath: 2,
-        size: 450,
-        position: {
-            lat: 37.362863347890716,
-            lng: -121.97802139023555,
-        },
-    },
-    {
-        address: "2019 Natasha Dr, San Jose, CA",
-        description: "Single family house",
-        price: "$ 2,325,000",
-        type: "home",
-        bed: 4,
-        bath: 3.5,
-        size: 500,
-        position: {
-            lat: 37.41391636421949,
-            lng: -121.94592071575907,
-        },
-    },
+    }
 ];
 
 initMap();
