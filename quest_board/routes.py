8c1f54b363e0447223b329acd44312490020fb7d
AndyWright360 <>
2024-02-03 17:39:46 +0000

    docs: add events.html page

--- quest_board/routes.py
+++ quest_board/routes.py
@@ -6,3 +6,13 @@ from quest_board.models import User, Event
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/create_event")
+def create_event():
+    return render_template("create_event.html")
+
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
