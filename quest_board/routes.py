c95834614d7494939e42cf9556b6e55e198ebac2
Andrew Wright <agwright360@gmail.com>
2024-03-05 21:06:18 +0000

    docs: add 404 error handler route function

--- quest_board/routes.py
+++ quest_board/routes.py
@@ -230,3 +230,8 @@ def bad_request_error(error):
 @app.errorhandler(403)
 def forbidden_error(error):
     return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
