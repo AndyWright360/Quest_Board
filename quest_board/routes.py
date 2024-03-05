53d582dd7ae00357db949216392a968af8562267
Andrew Wright <agwright360@gmail.com>
2024-03-05 21:12:13 +0000

    docs: add 500 error handler route function

--- quest_board/routes.py
+++ quest_board/routes.py
@@ -7,7 +7,7 @@ from quest_board.models import User, Event
 
 @app.route("/")
 def home():
-    return render_template("500.html")
+    return render_template("index.html")
 
 
 @app.route("/sign_up", methods=["GET", "POST"])
@@ -235,3 +235,8 @@ def forbidden_error(error):
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
