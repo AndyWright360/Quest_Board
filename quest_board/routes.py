76557184e5e86b83853702c1b41ecb03f5dc18e3
Andrew Wright <agwright360@gmail.com>
2024-03-05 20:56:42 +0000

    docs: add 403.html error page

--- quest_board/routes.py
+++ quest_board/routes.py
@@ -7,7 +7,7 @@ from quest_board.models import User, Event
 
 @app.route("/")
 def home():
-    return render_template("400.html")
+    return render_template("403.html")
 
 
 @app.route("/sign_up", methods=["GET", "POST"])
