a5544a4ae62c8fda33c41ce979ebcd6a2e0a8d7a
Andrew Wright <agwright360@gmail.com>
2024-03-06 21:41:55 +0000

    docs: create form.js file to remove event listener console error

--- quest_board/static/js/script.js
+++ quest_board/static/js/script.js
@@ -63,34 +63,6 @@ $(document).ready(function () {
     }
 });
 
-/*
-Performs a validation check on the text based event form inputs to ensure they contain characters.
-Also, prevents a text input from starting or finishing with whitespace.
-*/
-document.getElementById("event-form").addEventListener("submit", function(event) {
-    // Prevent form submission
-    event.preventDefault();
-
-    // Variables for text entry inputs
-    let eventName = document.getElementById("event_name").value;
-    let eventDescription = document.getElementById("description").value;
-
-    // Check if only whitespace has been entered in either input
-    if (eventName.trim() === '' || eventDescription.trim() === '' ) {
-        alert("Inputs must not be empty");
-        return;
-    }
-
-    // Check if either input starts or eb=nds with whitespace
-    if (eventName.trim() != eventName || eventDescription.trim() != eventDescription) {
-        alert("Inputs must not start or end with empty space");
-        return;
-    }
-
-    // If all validations pass, submit the form
-    event.target.submit();
-});
-
 // Create variable of footer icons
 const icons = document.getElementsByClassName("footer-icon");
 
