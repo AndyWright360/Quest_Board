1045a67a1d360e28db3d69fc5e2c00ebafd2ef4d
Andrew Wright <agwright360@gmail.com>
2024-03-08 21:30:05 +0000

    feat: validate that user has authorisation to delete or edit events

--- quest_board/routes.py
+++ quest_board/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash, session, url_for
+from flask import render_template, request, redirect, flash, session, url_for, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from quest_board import app, db
@@ -137,63 +137,77 @@ def create_event():
             db.session.commit()
             flash((f"Event '{event.event_name}' created"))
             return redirect(url_for("events"))
-        else:
-            flash("You need to be logged in to create an event")
-            return redirect(url_for("log_in"))  # Redirect to login page if user is not logged in
+
+        flash("You need to be logged in to create an event")
+        return redirect(url_for("log_in"))  # Redirect to login page if user is not logged in
     return render_template("create_event.html")
 
 
 @app.route("/edit_event/<int:event_id>", methods=["GET", "POST"])
 def edit_event(event_id):
     event = Event.query.get_or_404(event_id)
-    if request.method == "POST":
-        if "user" in session:  # Check if user is logged in
-            new_party_size = int(request.form.get("party_size"))
-            new_event_date = datetime.strptime(request.form.get("date"), "%d %b, %Y").date()
-            new_event_name = request.form.get("event_name")
-            new_description = request.form.get("description")
-            
-            # Check if the new party size is greater than or equal to the number of party members
-            if new_party_size < len(event.party_members):
-                flash("Cannot reduce party size below the number of joined members")
-                return redirect(url_for("edit_event", event_id=event_id))
-
-            # Check event date against current date
-            elif new_event_date < datetime.today().date():
-                flash("Event date cannot be in the past")
-                return redirect(url_for("edit_event", event_id=event_id))
-            
-            # Check if text inputs are only white spaces
-            elif new_event_name.strip() == "" or new_description.strip() == "":
-                flash("Inputs must not be empty")
-                return redirect(url_for("edit_event", event_id=event_id))
-            # Check if text inputs start or end with spaces
-            elif new_event_name != new_event_name.strip() or new_description != new_description.strip():
-                flash("Inputs must not start or end with spaces")
-                return redirect(url_for("edit_event", event_id=event_id))
-                    
-            event.party_size = new_party_size
-            event.event_name = new_event_name
-            event.location = request.form.get("location")
-            event.time = request.form.get("time")
-            event.date = new_event_date
-            event.description = new_description
-            event.exp_level = request.form.get("exp_level")
+    if "user" in session:  # Check if user is logged in
+        if session["user"] == event.created_by or session["user"] == "admin":
+            if request.method == "POST":
+                new_party_size = int(request.form.get("party_size"))
+                new_event_date = datetime.strptime(request.form.get("date"), "%d %b, %Y").date()
+                new_event_name = request.form.get("event_name")
+                new_description = request.form.get("description")
                 
-            db.session.commit()
-            flash("Event updated successfully")
-            return redirect(url_for("events"))
+                # Check if the new party size is greater than or equal to the number of party members
+                if new_party_size < len(event.party_members):
+                    flash("Cannot reduce party size below the number of joined members")
+                    return redirect(url_for("edit_event", event_id=event_id))
 
+                # Check event date against current date
+                elif new_event_date < datetime.today().date():
+                    flash("Event date cannot be in the past")
+                    return redirect(url_for("edit_event", event_id=event_id))
+                
+                # Check if text inputs are only white spaces
+                elif new_event_name.strip() == "" or new_description.strip() == "":
+                    flash("Inputs must not be empty")
+                    return redirect(url_for("edit_event", event_id=event_id))
+
+                # Check if text inputs start or end with spaces
+                elif new_event_name != new_event_name.strip() or new_description != new_description.strip():
+                    flash("Inputs must not start or end with spaces")
+                    return redirect(url_for("edit_event", event_id=event_id))
+                        
+                event.party_size = new_party_size
+                event.event_name = new_event_name
+                event.location = request.form.get("location")
+                event.time = request.form.get("time")
+                event.date = new_event_date
+                event.description = new_description
+                event.exp_level = request.form.get("exp_level")
+                    
+                db.session.commit()
+                flash("Event updated successfully")
+                return redirect(url_for("events"))
+
+            return render_template("edit_event.html", event=event)
+        else:
+            flash("You are not authorised to edit this event")
+            abort(403)  # Forbidden access
+    else:
         flash("You need to be logged in to edit an event")
         return redirect(url_for("log_in"))  # Redirect to login page if user is not logged in
-    return render_template("edit_event.html", event=event)
 
 
 @app.route("/delete_event/<int:event_id>")
 def delete_event(event_id):
-    event = Event.query.get_or_404(event_id)
-    db.session.delete(event)
-    db.session.commit()
+    if "user" in session:
+        event = Event.query.get_or_404(event_id)
+        if session["user"] == event.created_by or session["user"] == "admin":
+            db.session.delete(event)
+            db.session.commit()
+            flash("Event deleted successfully")
+        else:
+            flash("You are not authorised to delete this event")
+            abort(403) # Forbidden access
+    else:
+        flash("You need to be logged in to delete an event")
     return redirect(url_for("events"))
 
 
@@ -222,6 +236,7 @@ def join_event(event_id):
             return redirect(url_for('event', event_id=event_id))
         else:
             flash("User not found")
+            return redirect(url_for('event', event_id=event_id))
     else:
         flash("You need to be logged in to join an event")
         return redirect(url_for('log_in'))
@@ -237,8 +252,10 @@ def leave_event(event_id):
             event.party_members.remove(user)
             db.session.commit()
             flash(f"You have left '{event.event_name}'")
+            return redirect(url_for('event', event_id=event_id))
         else:
             flash("You are not currently a member of this event")
+            return redirect(url_for('event', event_id=event_id))
     else:
         flash("You need to be logged in to leave an event")
     return redirect(url_for('event', event_id=event_id))
